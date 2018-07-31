68d007f8c48d3ed98839c61e9717e9f13a9124fb
swat1508 <swatantrasinha1508@gmail.com>
2018-07-31 12:09:34 +0530

    Update login.controller.spec.js
    restoring success test case

--- tests/loginPage/login.controller.spec.js
+++ tests/loginPage/login.controller.spec.js
@@ -21,6 +21,6 @@ describe('Math', () => {
   // Test Two: A string explanation of what we're testing
   it('should test if (3-4)*8 = -8', () => {
     // Our actual test: (3-4)*8 SHOULD EQUAL -8
-    assert.equal(-8, (3 - 4) * 9);
+    assert.equal(-8, (3 - 4) * 8);
   });
 });
