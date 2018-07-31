562820600e3a1eec555b9ee4dd453cee3c7045cd
swat1508 <swatantrasinha1508@gmail.com>
2018-07-31 11:56:19 +0530

    Update login.controller.spec.js
    checking failure case

--- tests/loginPage/login.controller.spec.js
+++ tests/loginPage/login.controller.spec.js
@@ -7,7 +7,7 @@ describe('Array', () => {
     // A string explanation of what we're testing
     it('should return -1 when the value is not present', () => {
       // Our actual test: -1 should equal indexOf(...)
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+      assert.equal(-1, [1, 2, 3].indexOf(2));
     });
   });
 });
