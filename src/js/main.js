dbaf9dd4497358f79b5830e4c88104cad315858b
mohiit1502 <mohiit1502@gmail.com>
2018-08-01 15:06:00 +0530

    refactor

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import QuestionController from './controllers/questionController';
 require('./controllers/questionManagerController');
 require('../scss/main.scss');
-require('../scss/style.scss');
 
 const questionController = new QuestionController();
 // console.log(questionController);
