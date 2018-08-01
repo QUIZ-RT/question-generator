d6616f2032106e351e11973c1101f12e15af1de2
Swatantra Sinha <swasinha1@publicisgroupe.net>
2018-08-01 21:52:29 +0530

    resolving conflicts

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ app.use(bodyParser.json()); // parsing data or middleware to server
 
 app.use(express.static('dist'));// it will prefer the directory to serve the file
 
-<<<<<<< HEAD
 require('./src/js/routes/questionRoutes.js')(app)// for question related endpoints  
 require('./src/js/routes/topicRoutes.js')(app)// for topics related endpoints  
 require('./src/js/routes/questionManagerRoutes.js')(app)// for question related endpoints  
 // require('./routes/questionRoutes.js')(app)// for question related endpoints  
 // require('./routes/topicRoutes.js')(app)// for topics related endpoints  
 // require('./routes/questionManagerRoutes.js')(app)// for question related endpoints  
-=======
 const PORT = process.env.PORT || 8080;// finding the port number
->>>>>>> 33b3800ac925f47cb7ccffbc165bd8add6cd6f9b
 
 app.listen(PORT, () => {
   console.log('Listening on  port 8080');
