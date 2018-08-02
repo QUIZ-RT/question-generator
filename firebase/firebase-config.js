12eb85b09f3984d070fad3eddab2ab05ab334a23
techno2mahi <techno2mahi@gmail.com>
2018-08-02 18:02:44 +0530

    Syntax error fix

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,5 +1,5 @@
 
-module.exports = config = {
+const config = {
   apiKey: 'AIzaSyCYbFTLJktjYHMsK7Nx_m3UMJsw2Nq0O4Y',
   authDomain: 'quizgenx-884f0.firebaseapp.com',
   databaseURL: 'https://quizgenx-884f0.firebaseio.com',
@@ -7,3 +7,5 @@ module.exports = config = {
   storageBucket: 'quizgenx-884f0.appspot.com',
   messagingSenderId: '826554593648',
 };
+
+module.exports = config;
