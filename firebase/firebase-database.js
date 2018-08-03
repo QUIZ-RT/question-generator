722d934abe4fff0720aed7eb3fda62b8524247a1
unknown <sacjain1@WKWIN0707075.global.publicisgroupe.net>
2018-08-03 16:49:20 +0530

    login page added

--- firebase/firebase-database.js
+++ firebase/firebase-database.js
@@ -4,37 +4,72 @@ const firebaseInit = require('./firebase');
 require('firebase/database');
 
 module.exports = class firebaseDatabase {
-
+  generateRandomQuizId(name) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i += 1) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return (`${name}-${text}`);
+  }
   getFirebaseData(refUrl) {
     return firebaseInit.database().ref(refUrl).once('value').then((response) => {
       return response.val();
     });
   }
   saveFirebaseData(refUrl, postDataObj, resolve, reject) {
-    let promiseArray = [];
+    firebaseInit.database().ref(refUrl).set(postDataObj, (error) => {
+      if (error) {
+        reject('there is some issue we will come back sortly');
+      } else {
+        resolve('SuccessFully');
+      }
+    });
+  }
+  saveFirebaseArrayData(refUrl, postDataObj, resolve, reject) {
+    let promiseArray = [], refId = '';
+    if (refUrl === 'questions') {
+      refId = 'quiz';
+    } else if (refUrl = 'topics') {
+      refId = 'topic';
+    }
     for (let dataObj of postDataObj) {
-      promiseArray.push(new Promise((resolve, reject) => {
-        firebaseInit.database().ref(`${refUrl}/${dataObj.id}`).set(dataObj, (error) => {
+      promiseArray.push(new Promise((resolved, rejected) => {
+        if (refUrl === 'questions') {
+          let tempObj = {}
+          for (let i = 0; i < dataObj.options.length; i += 1) {
+            tempObj[this.generateRandomQuizId('opt')] = dataObj.options[i];
+          }
+          dataObj.options = tempObj;
+        }
+        firebaseInit.database().ref(`${refUrl} /${this.generateRandomQuizId(refId)}`).set(dataObj, (error) => {
           if (error) {
-            reject('there is some issue we will come back sortly');
+            rejected('there is some issue we will come back sortly');
           } else {
-            resolve(dataObj);
+            resolved(dataObj);
           }
         });
       })
       );
     }
-    promiseArray.all().then((savedResult) => {
+    Promise .all(promiseArray).then((savedResult) => {
       resolve(savedResult)
     }).catch((error) => {
       reject(error);
-    })
+    });
   }
   getCurrentUserInfo() {
     const userId = firebase.auth().currentUser.uid;
     return this.getFirebaseData(`/users/${userId}`);
   }
 
+  getUsers(userId) {
+    let refUrl = 'users';
+    if (userId) {
+      refUrl = `users/${userId}`;
+    }
+    return this.getFirebaseData(refUrl);
+  }
   getTopics(topicId) {
     let refUrl = 'topics';
     if (topicId) {
@@ -52,18 +87,17 @@ module.exports = class firebaseDatabase {
   }
 
   seveLoggedUserInfo(userId, loginObj, callback) {
-    const loginTempObj = { ...loginObj, isauthorized: false, isUserBlocked: false };
     const refUrl = `users/${userId}`;
-    this.saveFirebaseData(refUrl, loginTempObj, callback)
+    this.saveFirebaseData(refUrl, loginObj, callback)
   }
 
   saveTopics(topicObj, resolve, reject) {
-    const refUrl = 'topics/';
-    this.saveFirebaseData(refUrl, topicObj, resolve, reject);
+    const refUrl = 'topics';
+    this.saveFirebaseArrayData(refUrl, topicObj, resolve, reject);
   }
 
-  saveQuestions(quizId, quizObj, callback) {
-    const refUrl = `topics/${quizId}`;
-    this.saveFirebaseData(refUrl, quizObj, callback);
+  saveQuestions(quizObj, resolve, reject) {
+    const refUrl = 'questions';
+    this.saveFirebaseArrayData(refUrl, quizObj, resolve, reject);
   }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,13 @@
 import jQuery from 'jquery';
-import { MDCTopAppBar } from '@material/top-app-bar/index';
-import { MDCTemporaryDrawer, MDCTemporaryDrawerFoundation, util } from '@material/drawer';
-import QuestionController from './controllers/questionController';
+import { MDCTemporaryDrawer } from '@material/drawer';
 import { loadScreenRoute } from './shared/routes';
-
-// import { callGoogleSignIn } from '../../firebase/firebase-signin';
-import { getQuestions, getTopics } from '../../firebase/firebase-database';
 import 'popper.js';
 import 'bootstrap';
 import '../scss/main.scss';
 import './AjaxSetting';
+import authEventListener from './authentication';
 
+require('@material/top-app-bar/index');
 require('./controllers/questionManagerController');
 // require('./fcm-notification.js');
 
@@ -21,31 +18,10 @@ export function loadScreen(screen) {
   loadScreenRoute(screen);
 }
 jQuery(document).ready(() => {
-  jQuery.ajax({
-    type: 'post',
-    contentType: 'application/json',
-    dataType: 'json',
-    url: '/firebase/api/topics',
-    data: JSON.stringify(topic),
-  }).done((response) => {
-    console.log(response);
-  }).fail((jqXhr) => {
-    console.log(jqXhr);
+  jQuery('.navScreen').on('click', (e) => {
+    const current = e.currentTarget;
+    loadScreenRoute(jQuery(current).attr('data-screen'));
   });
 
-//   jQuery.ajax({
-//     type: 'post',
-//     contentType: 'application/json',
-//     dataType: 'json',
-//     url: '/firebase/signin',
-//   }).done((response) => {
-//     console.log(response);
-//   }).fail((jqXhr) => {
-//     console.log(jqXhr);
-//   });
-});
-
-jQuery('.navScreen').on('click', (e) => {
-  const current = e.currentTarget;
-  loadScreenRoute(jQuery(current).attr('data-screen'));
+  authEventListener();
 });
