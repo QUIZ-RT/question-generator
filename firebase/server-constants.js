55c169e9bcbadeb4bd12cae08c20ce967b347a8b
unknown <swatantrasinha1508@gmail.com>
2018-08-10 12:04:58 +0530

    for page size

--- firebase/server-constants.js
+++ firebase/server-constants.js
@@ -3,7 +3,8 @@ const ServerConstants = {
   NOTIFICATION_ADMIN_ACCESS: 'AdminAccessRequest',
   NOTIFICATION_TOPIC_UPDATE: 'NotificationTopicUpdate',
   ADMIN_ACCESS_REQUEST: 'AdminAccessRequest',  
-  FCM_TOKEN_UPDATE: 'FcmTokenUpdate'
+  FCM_TOKEN_UPDATE: 'FcmTokenUpdate',
+  PAGINATION_LIMIT: 11
 };
 
 module.exports = ServerConstants;
