e952a9220d333a51c9b71de09d397b86ac51f4b5
unknown <swatantrasinha1508@gmail.com>
2018-08-13 13:37:59 +0530

    changes for alert

--- src/js/AjaxSetting.js
+++ src/js/AjaxSetting.js
@@ -1,11 +1,10 @@
 const jQuery = require('jquery');
 
 jQuery(document).on('ajaxSend', () => {
-  const interval = setInterval(() => {
+  if(!jQuery('#ajaxSpinner').length){
+    jQuery('#ajaxSpinner').remove();
     jQuery('body').append("<div class='loading' id='ajaxSpinner'></div>");
-    clearInterval(interval);
-  }, 1);
-})
-  .on('ajaxComplete', () => {
+  }
+}) .on('ajaxStop',function() {
     jQuery('#ajaxSpinner').remove();
   });
